6bece6621c42028e47fb41188f99894d7dd67e4c
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-10-12 16:38:20 +0100

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ function App() {
 
   return (
     <>
-    Hello World
+   <div className="h-screen bg-black flex justify-center items-center">
+   <h1 className="text-4xl font-bold text-black-600 bg-white p-6 border border-black-600 rounded-lg ">
+    Hello World!
+  </h1>
+   </div>
     </>
   )
 }
