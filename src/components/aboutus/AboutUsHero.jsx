605971b3962fa7e084fa006ea89d7bad168a9d59
unknown <haithem.gouder@univ-constantine2.dz>
2024-11-15 11:26:08 +0100

    Implement WhoAreWe and OurValues sections with responsive styles

---
+++ src/components/aboutus/AboutUsHero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../../assets/styles/components/aboutUs/hero.css";
+
+const AboutUsHero = () => {
+  return (
+    <div className="about-hero">
+      <div className="about-hero-content">
+        <h1 className=".font-size-display">About Us</h1>
+        <p className=".font-size-paragraphs">
+          Lorem ipsum dolor sit amet consectetur. Risus et commodo pretium
+          ultrices erat diam turpis aliquet. Ullamcorper est pellentesque
+          venenatis odio sed ante.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUsHero;
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import Navbar from "../components/navbar";
+import AboutUsHero from "../components/aboutus/AboutUsHero";
+import WhoAreWe from "../components/aboutus/WhoAreWe";
 const About = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    return <main className="relative">
-        <Navbar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <main className="relative">
+      <Navbar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      <AboutUsHero />
+      <WhoAreWe />
     </main>
-}
-export default About
+  );
+};
+export default About;
